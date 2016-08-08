d9422bb0036616bcf486fc5f84fd2f4d2b08a1ed
Alan Russell <ajrussellaudio@gmail.com>
2016-08-08 15:41:46 +0100

    adds address JSON

--- controller.rb
+++ controller.rb
@@ -1,6 +1,18 @@
 require "sinatra"
 require "sinatra/contrib/all"
+require "json"
 
 get "/" do
   erb(:my_info)
+end
+
+get "/address" do
+  content_type( :json )
+  info = {
+    address: '3 ARGYLE HOUSE', 
+    building: 'CODEBASE', 
+    postcode: 'e13 zqf', 
+    phone: '0131558030'
+  }
+  return info.to_json()
 end
